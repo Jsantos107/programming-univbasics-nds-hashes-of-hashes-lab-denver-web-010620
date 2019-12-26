27c13c4fb3e12a51e55dda7d185ff1fbf01fcf14
Jorge Santos <neneluis55@gmail.com>
2019-12-26 20:24:35 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -1,3 +1,4 @@
+
 # Build a nested HoH of the classification of biological life on Earth based
 # on these commented-out snippets assigned to PORTION_1-PORTION_4. Return
 # that HoH as the return value for the method "naming_system."
@@ -30,7 +31,22 @@
 #   sub_category: nil
 # }
 
-def naming_system
+def naming_system{
+	          label: "Kingdom",
+	          sub_category: {
+           label: "Phylum",
+            sub_category: {
+              label: "Class",
+              sub_category:{
+	           label: "Order",
+                sub_category: { 
+               label: "Family",
+                  sub_category: {
+                    label: "Genus",
+                    sub_category: {
+                      label: "Species",
+                      sub_category: nil } } } } } } }
+
   # Remember:
   #  Kingdom
   #  Phylum
